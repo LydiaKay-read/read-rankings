32d08566baf2a86e4b7476c9849def86964e61c4
L Dawg <lydiakread@icloud.com>
2026-01-03 16:20:38 +0000

    Fix centering

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,48 @@
 // src/App.jsx
-import React from 'react'
+import React, { startTransition } from 'react'
 import './App.css'
 
 export default function App() {
-  const users = [
-    {id: 1, firstName: 'James', height: '6ft 1'},
-    {id: 2, firstName: 'Lydia', height: '5ft 9'},
-    {id: 3, firstName: 'Nic', height: '4ft 2'},
-  ]
+const users = [
+  { id: 1, firstName: 'Captain', lastName: 'Boring', height: '5ft 10', sex: 'Trans' },
+  { id: 2, firstName: 'Lydia', lastName: 'Read', height: '5ft 9', sex: 'F' },
+  { id: 3, firstName: 'Nic', lastName: 'Grinch', height: '4ft 3', sex: 'Lady boy' },
+];
 
   return (
     <div className="app-root">
       <main className="container">
-        <h1 className="title">Read Rankings</h1>
-
+        <h1 className="title">Lydia Read Rankings</h1>
         <div className="table-wrap" role="region" aria-label="Read Rankings table">
+    
           <table className="rank-table">
             <caption className="sr-only">List of users and their read rankings</caption>
-            <thead>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Height</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.firstName}</td>
-                  <td>{user.height}</td>
-                </tr>
-              ))}
-            </tbody>
+           <thead>
+  <tr>
+    <th scope="col">First Name</th>
+    <th scope="col">Last Name</th>
+    <th scope="col">Height</th>
+    <th scope="col">Sex</th>  
+  </tr>
+</thead>
+          <tbody>
+  {users.map((user) => (
+    <tr key={user.id}>
+      <td>{user.firstName}</td>
+      <td>{user.lastName}</td>
+      <td>{user.height}</td>
+      <td>{user.sex}</td>  
+    </tr>
+  ))}
+</tbody>
           </table>
+          
         </div>
       </main>
     </div>
   )
 }
+
+
+
+
